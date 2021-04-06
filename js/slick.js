e2d57cb7934855293320cc243b2f6dcae1a3cca7
Mr-1-GX <76249374+Mr-1-GX@users.noreply.github.com>
2021-04-06 15:45:52 +0400

    add slide

--- js/slick.js
+++ js/slick.js
@@ -4,4 +4,43 @@ $('.autoplay').slick({
     autoplay: true,
     autoplaySpeed: 2000,
   });
+
+  $('.center').slick({
+    centerMode: true,
+    centerPadding: '80px',
+    slidesToShow: 4,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '20px',
+          slidesToShow: 3
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '20px',
+          slidesToShow: 1
+        }
+      }
+    ]
+  });
+
+
+  $('.lazy').slick({
+    lazyLoad: 'ondemand',
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+  });
+
+  
       
