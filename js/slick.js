43895255bce01a6c9e2b6c17cb9f0386dde2c045
Mr-1-GX <76249374+Mr-1-GX@users.noreply.github.com>
2021-04-08 18:28:36 +0400

    add pages

--- js/slick.js
+++ js/slick.js
@@ -51,4 +51,13 @@ $('.lazy').slick({
   centerPadding: '50px',
 });
 
+$('.autoplay').slick({
+  dots: true,
+  arrows: false,
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 2000,
+});
+
       
