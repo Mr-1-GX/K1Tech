3ca7992a9e0afc7f57805e59b9d9023fbaa626a7
Mr-1-GX <76249374+Mr-1-GX@users.noreply.github.com>
2021-05-07 18:04:30 +0400

    add head style

--- js/script.js
+++ js/script.js
@@ -4,4 +4,14 @@ $("#burger-menu").on("click", function(){
     $("#socicon").toggleClass("open-socicon");
   })
 
-  
+const header = document.querySelector('.head-cont');
+
+window.onscroll = function(){
+  var top = window.scrollY;
+  console.log(top);
+  if (top >= 70){
+    header.classList.add('active')
+  }else {
+    header.classList.remove('active');
+  }
+}
