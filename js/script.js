f91a5a7caee73cf3dda7ac9afa814d434c035fdf
Mr-1-GX <76249374+Mr-1-GX@users.noreply.github.com>
2021-05-13 18:00:45 +0400

    add responsive

--- js/script.js
+++ js/script.js
@@ -10,7 +10,7 @@ const header = document.querySelector('.head-cont');
 window.onscroll = function(){
   var top = window.scrollY;
   console.log(top);
-  if (top >= 70){
+  if (top >= 30){
     header.classList.add('active')
   }else {
     header.classList.remove('active');
