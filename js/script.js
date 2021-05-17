674b89c086c939c0a4276ee6fbcba63cdf7f2457
Mr-1-GX <76249374+Mr-1-GX@users.noreply.github.com>
2021-05-17 16:46:55 +0400

    add mobile responsive

--- js/script.js
+++ js/script.js
@@ -3,6 +3,7 @@ $("#burger-menu").on("click", function(){
     $("#menu").toggleClass("open-menu");
     $("#socicon").toggleClass("open-socicon");
     $("#head-size").toggleClass("open-head-size");
+    $("#sect-2-flt-size").toggleClass("open-sect-2-flt-size");
   })
 
 const header = document.querySelector('.head-cont');
